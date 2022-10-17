2ce93226f746da924a984ae6119691e7de50a1a5
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2022-10-17 03:10:05 +0300

    23._Merge_k_Sorted_Lists

---
+++ 23._Merge_k_Sorted_Lists.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        #not fast but implementing it using Heap
+#         arr = []
+        
+#         for i in lists:
+#             while i:
+#                 arr.append(i.val)
+#                 i = i.next
+#         # print(arr)
+#         heapq.heapify(arr)
+#         d = dummy = ListNode()
+        
+#         for i in range(len(arr)):
+#             s = heapq.heappop(arr)
+#             d.next = ListNode(s)
+#             d = d.next
+#         return dummy.next
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
