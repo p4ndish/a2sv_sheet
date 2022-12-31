e6dca27def6989e51faf21b4ffd882380c5064ef
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2022-12-31 15:27:18 -0500

    1512._Number_of_Good_Pairs

--- 1512._Number_of_Good_Pairs.py
+++ 1512._Number_of_Good_Pairs.py
@@ -2,21 +2,10 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         res = 0
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j] and i < j:
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
                     res += 1
         return res
-​
-#         from itertools import combinations
-#         s= combinations(nums,2)
-#         # print(s)
-#         count = 0
-#         for i in list(s):
-#             # print(i[0])
-#             if i[0] == i[1]:
-#                 count += 1
-#             # print(i)
-#         return count
         
         
         
@@ -31,9 +20,14 @@ class Solution:
         
         
         
-        
-        
-        
+#         c = defaultdict()
+#         res = 0
+#         for i in nums:
+#             if i in c:
+#                 res += 1
+#             else:
+#                 c[i] = 1
+#         return res
         
         
         
