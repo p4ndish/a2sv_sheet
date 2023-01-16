355d35842dfd862b21baddbdcc86362ce0498bd1
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-01-16 21:00:27 +0300

    Create problem_1.py

---
+++ contest/problem_1.py
@@ -0,0 +1,34 @@
+from collections import Counter
+s = str(input())
+ 
+res = ""
+idx = 0
+c = Counter("hello")
+for i in range(len(s)):
+    if s[i] == 'h' and res == "":
+        res += s[i]
+        idx += 1
+        c[s[i]] -= 1
+    elif s[i] == "e" and res and res[idx-1] == 'h' and c[s[i]] > 0:
+        res += s[i]
+        idx += 1
+        c[s[i]] -= 1
+    elif s[i] == 'l' and res and res[idx-1] == 'e' and c[s[i]] > 0:
+        res += s[i]
+        c[s[i]] -= 1
+        idx += 1
+    elif s[i] == 'l' and res and res[idx-1] == 'l' and c[s[i]] > 0:
+        res += s[i]
+        idx += 1
+        c[s[i]] -= 1
+    elif s[i] == 'o' and res and res[idx-1] == 'l' and res[idx-2] == 'l' and c[s[i]] > 0:
+        c[s[i]] -= 1
+        res += s[i]
+        idx += 1
+        
+if len(res) == 5:
+    print("YES")
+else:
+    print("NO")
+    
+        
