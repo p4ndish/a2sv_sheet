1564327e111f4b941f8f558004067977796d14a3
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-01-25 15:46:01 -0500

    1365._How_Many_Numbers_Are_Smaller_Than_the_Current_Number

--- 1365._How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
+++ 1365._How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -1,12 +1,11 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        arr = [0]*len(nums)
-        
-        for i in range(0,len(nums)):
-            for j in range(0, len(nums)):                 
-                if nums[i] > nums[j]:
-                    try:
-                        arr[i] +=1
-                    except:
-                        pass
-        return arr
+        cnt = []
+        # nums.sort()
+        for i in range(len(nums)):
+            c = 0
+            for j in range(len(nums)):
+                if j != i and nums[i] > nums[j]:
+                    c += 1
+            cnt.append(c)
+        return cnt
