2d6b559442acf45d7fcf956553fde203ec1bc552
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-01-29 14:00:37 -0500

    179._Largest_Number

--- 179._Largest_Number.py
+++ 179._Largest_Number.py
@@ -1,17 +1,14 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        # nums.sort()
-        if len(nums) <= 1:
-            return "".join(map(str,nums))
-        else:
-            # def cmp(x,y):
-            #     if x+y < y+x:
-            #         return 1
-            #     else:
-            #         return -1
-            sotor = sorted(map(str,nums), key=cmp_to_key(lambda x,y: 1 if x+y < y+x else -1))
-            if sotor[0] == "0":
-                return "0"
+        def sorter(x,y):
+            if x+y > y+x:
+                return -1
+            elif y+x > x+y:
+                return 1
             else:
-                return "".join(sotor)
-                
+                return 0
+            
+        
+        ssort = sorted(map(str, nums), key=cmp_to_key(sorter))
+        print(ssort)
+        return "".join(ssort) if ssort[0] != "0" else "0" 
