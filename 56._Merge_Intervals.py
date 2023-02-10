1addd5bc14436232cd645aa306fcc29578f246fa
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-02-11 02:27:47 +0300

    56._Merge_Intervals

--- 56._Merge_Intervals.py
+++ 56._Merge_Intervals.py
@@ -1,16 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) == 1:
-            return intervals
-        intervals = sorted(intervals)
-        result = []
-        start , end = intervals[0][0],intervals[0][1]
+        intervals.sort(key=lambda x: x[0])
         
-        for i in intervals:
-            if i[0] <= end:
-                end = max(i[1],end)
+        start, end = intervals[0]
+        res = [intervals[0]]
+        for st, en in intervals[1:]:
+            if st <= end:
+                #error so make the end to "en"
+                end = max(en, end)
+                res[-1][1] = end
             else:
-                result.append([start, end])
-                start,end = i[0], i[1]
-        result.append([start,end])
-        return result
+                res.append([st,en])
+                start = st
+                end = en
+        print(res)
+        return res
