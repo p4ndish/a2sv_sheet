61dd4d6fce1918173f8079c157036f1d3a7ed170
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-02-11 23:08:14 +0300

    763._Partition_Labels

--- 763._Partition_Labels.py
+++ 763._Partition_Labels.py
@@ -1,25 +1,19 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        c = {v:i for i,v in enumerate(s)}
-        # print(c)
-        size = 0
-        end = 0
-        res = []
+        occ = dict()
         for i in range(len(s)):
-            if c[s[i]] > end:
-                end = c[s[i]]
-            size += 1
+            occ[s[i]] = i
+        print(occ)
+        start, end = 0, 0
+        res = []
+        
+        for i in range(len(s)):
+            end = max(end, occ[s[i]])
+            # print(res, end)
             if i == end:
-                res.append(size)
-                size = 0
-        return res
+                # print("if -> ",i, res, end)
+                res.append( end - start +1)
+                start = i + 1
                 
-    
-    
-    
-    
-    
-"""
-012345678 9
-ababcbaca d e f e g d e h i j h k l i j
+        return res
 ​
