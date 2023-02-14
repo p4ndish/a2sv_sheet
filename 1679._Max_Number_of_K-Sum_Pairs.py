2e99bf8b3026eb0ec982e6b47f95e04c223f390d
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-02-14 22:25:45 +0300

    1679._Max_Number_of_K-Sum_Pairs

--- 1679._Max_Number_of_K-Sum_Pairs.py
+++ 1679._Max_Number_of_K-Sum_Pairs.py
@@ -1,16 +1,33 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
-        count = 0
-        l,r = 0,len(nums)-1
-        while l<r:
-            if nums[l]+nums[r]==k:
-                count+=1
-                l+=1
-                r-=1
-            elif nums[l]+ nums[r]> k:
-                r-=1
+        N = len(nums)
+        l = 0
+        r = N - 1 
+        res = 0
+        while l < r:
+            x = nums[l] + nums[r]
+            if x > k :
+                r -= 1
+            elif x < k:
+                l += 1
             else:
-                l+=1
-        return count
-    
+                res += 1
+                l += 1
+                r -= 1
+        print(res)
+        return res
+        # d = defaultdict(int)
+        # res = 0
+        # s = ""
+        # for i in range(len(nums)):
+        #     x = k - nums[i]
+            
+        #     if x in d and d[x] > 0 and str(i) not in  s:
+        #         s += str(i)
+        #         # print(x, d)
+        #         d[x] -= 1
+        #         if d[x] <= 0: del d[x]
+        #         res += 1
+        #     if str(i) not in  s:
+        #         d[nums[i]] += 1
