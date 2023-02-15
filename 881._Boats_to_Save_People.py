2c0bbb9920152221e15848efcf72e632487a5805
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-02-16 01:38:32 +0300

    881._Boats_to_Save_People

--- 881._Boats_to_Save_People.py
+++ 881._Boats_to_Save_People.py
@@ -1,39 +1,24 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        '''
-        if you think dagim, if the arr is sorted and the first element and the last element can't 
-        make a pair then there is no way it can pair with the other small elements because it's
-        increasing... so if you i and j are less or qual to limit both pointers move to middle 
-        else the last element only will have it's own seat 
-        '''
         people.sort()
-        print(people)
-        i = 0
-        j = len(people)-1
-        c = 0 
-        
-        while i <= j:
-            if people[i] + people[j] <= limit:
-                c+=1
-                j-=1
-                i+=1
+        res = 0
+        s = set()
+        c = Counter(people)
+        N = len(people)
+        l = 0
+        r = N - 1
+        while l <= r:
+            f = people[l] + people[r]
+            if f <= limit :
+                res += 1
+                l += 1
+                r -= 1
             else:
-                if people[j] <= limit:
-                    c+=1
-                    j-=1
+                res += 1
+                if people[l] > people[r]:
+                    l += 1
                 else:
-                    j-=1
+                    r -= 1
                 
-        print(c)
-        return c
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return res
+            
