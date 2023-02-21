6319a387a0b2b08fc812cc29ba36b15663e325f0
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-02-21 22:39:40 +0300

    3._Longest_Substring_Without_Repeating_Characters

--- 3._Longest_Substring_Without_Repeating_Characters.py
+++ 3._Longest_Substring_Without_Repeating_Characters.py
@@ -1,44 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        i,j = 0, 0 
-        t = ""
-        m = 0
-        while j < len(s):
-            if s[j] in t:
-                t = s[i:j]
-                i += 1
+        left , right = 0, 0 
+        check = ""
+        res = 0
+        #c = Counter(s)
+        
+        N = len(s)
+        while right < N:
+            if s[right] in check:
+                check = s[left:right]
+                left += 1
             else:
-                t+=s[j]
-                j+=1
-            m = max(m,len(t))
-        return m
-        print(m)
-            
-                
+                check += s[right]
+                right += 1
+            res = max(res, len(check))
         
-          
-            
-            
-            
-            
-            
-            
-#         i = 0 
-#         j = 0
-#         c = 0
-#         visited = ""
-#         while j < len(s):
-#             print(visited)
-#             if s[j] in visited:
-#                 visited=s[i:j]
-#                 i+=1
-#             if s[j] not in visited:
-#                 visited+=s[j]
-#                 j+=1
-#             if len(visited) > c:
-#                 c = len(visited)
-                
-#         print(c)
-#         return c
-        
-​
+        return res
