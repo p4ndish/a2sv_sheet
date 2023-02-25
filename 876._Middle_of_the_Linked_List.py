81d353ae309f766d3d9dd207d85c6ccc5ffab9aa
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-02-25 20:53:25 +0300

    876._Middle_of_the_Linked_List

--- 876._Middle_of_the_Linked_List.py
+++ 876._Middle_of_the_Linked_List.py
@@ -5,35 +5,10 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
+        slow = head
+        fast = head
+        print(fast)
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         return slow
-        #approch two
-​
-        # slow,fast = head,head.next
-        # while fast:
-        #     slow = slow.next
-        #     if fast.next:
-        #         fast = fast.next.next
-        #     else:
-        #         fast = None
-        # return slow
-        
-        
-    
-#         node = ListNode()
-#         n = node
-#         s = 0
-#         curr = head
-#         while curr:
-#             curr = curr.next
-#             s+=1
-#         mid = s//2
-#         cur = head
-#         res = []
-#         while cur:
-#             if mid > 0:
-#                 mid-=1
-#             else:
