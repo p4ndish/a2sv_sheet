bb3e07cc22062e96c0b13408370da5d13f39c5d2
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-02-28 00:35:42 +0300

    82._Remove_Duplicates_from_Sorted_List_II

--- 82._Remove_Duplicates_from_Sorted_List_II.py
+++ 82._Remove_Duplicates_from_Sorted_List_II.py
@@ -5,34 +5,33 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
-        while head:
-            arr.append(head.val)
-            head = head.next
-        farr = []
-        dummy = ListNode()
-        d = dummy
-        for i in arr:
-            if arr.count(i) == 1:
-                d.next = ListNode(i)
-                d = d.next
-​
-        return dummy.next
-        #approch 2
-#         dummy = ListNode()
-#         dummy.next = head
-#         prev = dummy
-#         curr = head
-#         while curr :
-#             while curr.next and curr.val == curr.next.val:
-#                 curr = curr.next
-#             if prev.next == curr:
-#                 prev = curr
-#                 curr = curr.next
-#             else:
-#                 prev.next = curr.next
-#                 curr = curr.next
-#         return dummy.next
-                
+        
+        if not head or not head.next:
+            return head
+        
+        #step 1 
+        prev = head
+        curr = head.next
+        while curr:
+            if prev.val == curr.val or prev.val-1000 == curr.val:
+                prev.val += 1000
+                curr.val += 1000
+            prev = prev.next
+            curr = curr.next
             
-    
+        #step 2 
+        
+        while head and head.val > 100:
+            head = head.next
+        
+        if not head:
+            return head
+        
+        #step 3 
+        cur = head
+        while cur and cur.next:
+            if cur.next.val > 100:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+                
