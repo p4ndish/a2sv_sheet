1f9beb525a735d20fad08a926f925008608e77ce
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-03-08 17:05:24 +0300

    209._Minimum_Size_Subarray_Sum

--- 209._Minimum_Size_Subarray_Sum.py
+++ 209._Minimum_Size_Subarray_Sum.py
@@ -1,36 +1,18 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        mn = float('inf')
+        left = right = 0 
+        mnl = float(inf)
         
-        l = 0
-        # j = 0
-        curr = 0
-        print(nums)
-        for r in range(len(nums)):
+        sums = 0
+        while right < len(nums):
+            sums += nums[right]
+            while sums >= target and left < right:
+                mnl = min(mnl, (right-left+1))
+                sums -= nums[left]
+                left += 1 
+            if sums >= target:
+                mnl = min(mnl, (right-left+1))
+            right += 1 
             
-            curr+=nums[r]
-            while curr >= target:
-                mn = min(mn,r- l +1 )
-                curr -= nums[l]
-                l+=1
-        return mn if mn != inf else 0
-                
-​
-​
-        
-#         count = {0:1}
-#         res = float("inf")
-#         sums = 0
-#         for i in range(len(nums)):
-#             sums += nums[i]
-#             if sums - target in count :
-#                 # print(count, sums-target)
-#                 l = 0
-#                 tmp = sums
-#                 while tmp != target:
-#                     tmp -= nums[l]
-#                     # nums[l] += 1
-#                     l+=1
-                
-#                 res = min(res, (len(nums)-l))
-#                 # count[sums-target] += 1
+        # print(mnl)
+        return mnl if mnl != inf else 0 
