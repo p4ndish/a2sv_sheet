55e9cb9414d3dab8c88adb151469477da7e9a3d2
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-03-12 23:11:06 +0300

    2526._Find_Consecutive_Integers_from_a_Data_Stream

--- 2526._Find_Consecutive_Integers_from_a_Data_Stream.py
+++ 2526._Find_Consecutive_Integers_from_a_Data_Stream.py
@@ -1,19 +1,24 @@
 class DataStream:
 ​
     def __init__(self, value: int, k: int):
-        self.k = k
+        self.arr = []
         self.value = value
-        self.n = 0
+        self.k = k 
+        self.cont = 0
+        self.length = 0 
     def consec(self, num: int) -> bool:
-        
+        self.arr.append(num)
+        self.length += 1 
         if self.value == num:
-            self.n += 1
+            self.cont += 1 
         else:
-            self.n = 0
-            
-        return self.n >= self.k
+            self.cont = 0 
+        if self.length < self.k:
+            return False
+        if self.k <= self.cont:
+            return True
         
-                
+​
 ​
 # Your DataStream object will be instantiated and called as such:
 # obj = DataStream(value, k)
