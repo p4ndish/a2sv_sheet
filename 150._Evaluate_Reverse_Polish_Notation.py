17d419ecf280c26bb1189682a197eec8d0dad202
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-03-19 00:22:59 +0300

    150._Evaluate_Reverse_Polish_Notation

--- 150._Evaluate_Reverse_Polish_Notation.py
+++ 150._Evaluate_Reverse_Polish_Notation.py
@@ -1,24 +1,25 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
+        for i in range(len(tokens)):
+            if tokens[i] in "+-*/":
         
-        for i in tokens:
-            if i in "+-*/":
-                x = stack.pop()
-                y = stack.pop()
-                ans = self.verify(i,int(y),int(x))
-                stack.append(ans)
+                
+                p1 = stack.pop()
+                p2 = stack.pop()
+                s = self.polish(int(p1),int(p2), tokens[i])
+                stack.append(s)
             else:
-                stack.append(int(i))
-        return stack[0]
+                # print(stack, tokens[i])
+                stack.append(int(tokens[i]))
         # print(stack)
-    def verify(self, opr, y, x):
-        # print(x,y)
-        if opr == '+':
-            return y + x
-        elif opr == '-':
-            return y - x
-        elif opr == '*':
-            return y * x
-        else:
-            return int(y/x)
+        return int(stack[0]) 
+    def polish(self, p1, p2, sy):
+            if sy == '+':
+                return  p2 + p1
+            elif sy == '-':
+                return p2 - p1
+            elif sy == '*':
+                return p2 * p1
+            else:
+                return int(p2 / p1)
