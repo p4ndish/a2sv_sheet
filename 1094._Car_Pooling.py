3d305c742fcb39dc5cc282a8e716b617574080d9
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-05-03 00:44:01 +0300

    1094._Car_Pooling

--- 1094._Car_Pooling.py
+++ 1094._Car_Pooling.py
@@ -1,24 +1,30 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        '''
-        0 1 2 3 4 5 6 7  = 5
-          2 0 0 0-2
-              3 0 0 0-3
-          2 2 5 5 3 3 0
-        '''
+        trips.sort(key=lambda x: (x[2]))
+        prefix = [0] * (trips[-1][2] + 1)
         
-        prefix = [0]*1001
-        for trip in trips:
-            passangers, begin, end = trip
-            prefix[begin] += passangers
-            prefix[end] -= passangers
-            
-        # sums = prefix[0]
-        if prefix[0] > capacity:
-            return False
-        for i in range(1, len(prefix)):
-            prefix[i] += prefix[i-1] 
+        for i in range(len(trips)):
+            pass_, from_, to_ = trips[i]
+            prefix[from_] += pass_
+            prefix[to_] -= pass_
+        prefix = list(accumulate(prefix))
+        
+        for i in range(len(prefix)):
+            # prefix[i] += prefix[i-1]
             if prefix[i] > capacity:
                 return False
         return True
+        
+        print(trips,prefix)
+                   
+    
+    
+'''
+[[2,1,5],[3,3,7]]
 ​
+1 2 3 4 5 6 7 8
+2        -2
+    3        -3
+    
+2 2 5 5 5 3 3 0
+'''
