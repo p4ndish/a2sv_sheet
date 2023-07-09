67c5b402f317901431670776cea92e7ec8ea8f2f
/ye_k0lo_tem4ri <65342438+p4ndish@users.noreply.github.com>
2023-07-09 20:30:31 +0000

    1971._Find_if_Path_Exists_in_Graph

--- 1971._Find_if_Path_Exists_in_Graph.py
+++ 1971._Find_if_Path_Exists_in_Graph.py
@@ -1,19 +1,22 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
+        def find(node):
+            if node == parent[node]:
+                return node
+            
+            return find(parent[node])
         
-        if not edges:
-            return True
-        graph = defaultdict(list)
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-            
-            
+        def union(node1, node2):
+            par1, par2 = find(node1), find(node2)
+            if par1 != par2:
+                parent[par1] = parent[par2]
         
-            
-        def bfs(source, dest):
-            visited = set()
-            visited.add(source)
-            q = deque([source])
-            
+        parent = [i for i in range(n)]
+                
+                
+        for x, y in edges:
+            union(x, y)
+                      
+        return find(source) == find(destination)
+                
